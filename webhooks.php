4a19254a775538877751f4131bd3c43ef5fdfb5d
sittidate.s <sittidate.s@cattelecom.com>
2019-05-22 14:22:08 +0700

    แก้ access token

--- webhooks.php
+++ webhooks.php
@@ -3,7 +3,7 @@
 require "vendor/autoload.php";
 require_once('vendor/linecorp/line-bot-sdk/line-bot-sdk-tiny/LINEBotTiny.php');
 
-$access_token = '3ALKAbKFoGuJyJnoDdn0HeyfbxLFtEXBKiC0lFeoNl/XbL4WhoCZzefp2n7UDuXaCWfErIDro07BnZNggJmXJChXTIlMPo8LRJ+n1LEgbRUaKehDkiCr5p5CakHrPX+gauOGX/R5bB2e5yi7xjnHDAdB04t89/1O/w1cDnyilFU=';
+$access_token = 'F/JX8O0tOwNBc1ROgLsgoclGotP0e2o08xy6tbjV+XiQ0Ch7lbhrMQQn+PV8O9577aFKHbwBvAnNAC72jy608KpCHzwucSOb7dC1GO7UtvAEgDcuTvAA6H96fRowC1NdaxgElNXEwHq3bHSsnkurRAdB04t89/1O/w1cDnyilFU=';
 
 // Get POST body content
 $content = file_get_contents('php://input');
