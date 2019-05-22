ab88ddd9111873e064da6c3d9b98c7c0073b51e3
sittidate <13657349+sittidate@users.noreply.github.com>
2019-05-22 16:43:05 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -53,8 +53,8 @@ if (!is_null($events['events'])) {
 			];
 
 			// Make a POST Request to Messaging API to reply to sender
-			//$url = 'https://api.line.me/v2/bot/message/reply';
-			$url = 'https://api.line.me/v2/bot/message/broadcast';
+			$url = 'https://api.line.me/v2/bot/message/reply';
+			//$url = 'https://api.line.me/v2/bot/message/broadcast';
 			$data = [
 				'replyToken' => $replyToken,
 				'messages' => [$messages],
