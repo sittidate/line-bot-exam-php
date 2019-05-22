9f0fdfaebce56e963c968995b021221734743e47
sittidate <13657349+sittidate@users.noreply.github.com>
2019-05-22 16:02:30 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -40,7 +40,7 @@ if (!is_null($events['events'])) {
 		if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
 			// Get text sent
 			$text = $event['source']['userId'];
-			$text = "@".$event['source']['userId'];
+			$text = $event['message']['text'];
 			
 			// Get replyToken
 			$replyToken = $event['replyToken'];
